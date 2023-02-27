9a943b8df48106ba7e600943e6439e47fb2cc8cb
longo2002 <lombardo.fra2002@gmail.com>
2023-02-27 15:34:31 +0100

    Done

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const { createApp } = Vue
+
+
+createApp({
+    data() {
+        return {
+            mail: [],
+        }
+    },
+    methods: {
+        mailGenerator() {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                    console.log(res.data.response)
+                    this.mail.push(res.data.response.trim())
+                })
+        },
+    },
+    mounted() {
+        for (let i = 0; i < 10; i++) {
+            this.mailGenerator()
+        }
+    },
+}).mount('#app')
