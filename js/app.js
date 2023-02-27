80254b21435a863b5bc2f6111320aa55912d6183
longo2002 <lombardo.fra2002@gmail.com>
2023-02-27 15:46:05 +0100

    polish

--- js/app.js
+++ js/app.js
@@ -10,16 +10,19 @@ createApp({
     methods: {
         mailGenerator() {
             axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then((res) => {
                     console.log(res.data.response)
                     this.mail.push(res.data.response.trim())
                 })
         },
+        mailGeneratorDelayed(){
+            setTimeout(this.mailGenerator, 1000)
+        }
     },
     mounted() {
         for (let i = 0; i < 10; i++) {
             this.mailGenerator()
         }
     },
-}).mount('#app')
+}).mount("#app")
